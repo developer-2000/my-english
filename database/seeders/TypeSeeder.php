296c1c6c65698bdd4733bc8fb394b0c4578c8c90
багалов шамиль <thwglobal2000@gmail.com>
2024-04-05 19:09:58 +0300

    добавлено существительное в типы слов

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -24,6 +24,19 @@ class TypeSeeder extends Seeder {
             ['type'=>'подлежащее','color'=>'#28a745','description'=>$object],
         );
 
+        $object->text = 'Имеют единственное и множественное число с приставкой “s” в конце. <br/>
+Они используются с (the) или (a, an) артиклями - "a book" или "the car". <br/>
+"the" когда речь идет о объекте, который известен или упоминается ранее - "She bought the book". <br/>
+Артикль не ставится перед: <br/>
+Множественным числом и после числительного "have boys and one girl" <br/>
+После притяжательных местоимений "my, your, our, their, his, her" <br/>
+"a" ставится перед согласной буквой "I need a new computer." <br/>
+"an" ставится перед гласной "an interesting movie"
+';
+        WordType::create(
+            ['type'=>'существительное','color'=>'#cc9a03','description'=>$object],
+        );
+
         $object->text = null;
         $object->object = [
             "past"=>["word"=>null, "translation"=>null, "accent"=>null],
@@ -32,7 +45,7 @@ class TypeSeeder extends Seeder {
         ];
 
         WordType::create(
-            ['type'=>'формы времени','color'=>'#86751d','description'=>$object]
+            ['type'=>'формы времени','color'=>'#dc3545','description'=>$object]
         );
     }
 }
